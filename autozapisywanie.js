7c357c22a30119cadb7efd4387113adc03dbc989
Szymon Wasik (Malik) <szymon.wasik890@gmail.com>
2023-07-04 17:03:05 +0200

    Add files via upload

---
+++ autozapisywanie.js
@@ -0,0 +1,32 @@
+﻿const form = document.querySelector('form');
+
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email');
+const inputPhone = document.querySelector('#phone');
+
+const saveFormData = () => {
+  const formData = {
+    name: inputName.value,
+    email: inputEmail.value,
+    phone: inputPhone.value,
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+
+const loadFormData = () => {
+  const savedData = localStorage.getItem('formData');
+
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    inputName.value = formData.name;
+    inputEmail.value = formData.email;
+    inputPhone.value = formData.phone;
+  }
+};
+
+inputName.addEventListener('input', saveFormData);
+inputEmail.addEventListener('input', saveFormData);
+inputPhone.addEventListener('input', saveFormData);
+
+document.addEventListener('DOMContentLoaded', loadFormData);
